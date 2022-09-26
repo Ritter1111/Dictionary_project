65ff684cf6d31629019cbaa7be37c0657d4bdbf7
Ritter1111 <86166867+Ritter1111@users.noreply.github.com>
2022-09-26 13:24:56 +0300

    update API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
@@ -22,8 +24,9 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeyword} />
+        <input type="search" onChange={handleKeyword} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
