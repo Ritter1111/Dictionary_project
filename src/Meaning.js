29d23b33822b87f59ad2703036ae58d5d4c9a2b0
Ritter1111 <86166867+Ritter1111@users.noreply.github.com>
2022-09-27 12:52:09 +0300

    fixed API key

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,19 +3,29 @@ import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
+  function synonyms() {
+    if (props.meaning.synonyms) {
+        return <ul className="synonyms">
+            {props.meaning.synonyms.map((el, index) => {
+                return <li key={index}>{el}</li>;
+            })}
+        </ul>
+    }
+}
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
+      <strong>Synonyms:</strong>
+      {synonyms()}
+      {props.meaning.definitions.map(function (definit, index) {
         return (
           <div key={index}>
             <p>
               <strong>Definition:</strong>
-              {definition.definition}
+              {definit.definition}
               <br />
               <strong>Example:</strong>
-              <em>{definition.example}</em>
-              <Synonyms synonyms={definition.synonyms} />
+              <em>{definit.example}</em>
             </p>
           </div>
         );
